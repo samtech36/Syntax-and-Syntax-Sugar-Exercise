979eb644bc13bdb1b401f8947a58a12ad3892ce0
samtech36 <samuel.hossain@outlook.com>
2024-08-14 14:44:37 -0400

    my syntax and syntax sugar exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         { 
-           //int answer = 4;
-          //string response;
+           //int answer = 4; //explicit typing
+          //string response; //explicit typing
            
        // if (answer < 9) 
        //     {
@@ -16,7 +16,7 @@
          //       response = answer + " is greater than or equal to nine";
         //    }
 
-            var answer = 4;
+            var answer = 12; //inferred typing
 
             var response = (answer < 9) ? $"{answer} is less than nine" : $" {answer} is greater than or equal to nine";
             
--- SyntaxAndSyntaxSugarExercise/obj/Debug/net6.0/SyntaxAndSyntaxSugarExercise.AssemblyInfo.cs
+++ SyntaxAndSyntaxSugarExercise/obj/Debug/net6.0/SyntaxAndSyntaxSugarExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44cafacad74ce05fbe9c6997c716bed70b593155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b49bda90ff33222f4001305e2de941c2afccfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
