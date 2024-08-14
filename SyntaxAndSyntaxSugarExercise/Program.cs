f1b49bda90ff33222f4001305e2de941c2afccfb
samtech36 <samuel.hossain@outlook.com>
2024-08-14 13:02:08 -0400

    my syntax and syntax sugar exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -3,8 +3,24 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+           //int answer = 4;
+          //string response;
+           
+       // if (answer < 9) 
+       //     {
+        //      response = answer + " is less than nine";
+        //    }
+         //   else
+        //    {
+         //       response = answer + " is greater than or equal to nine";
+        //    }
+
+            var answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $" {answer} is greater than or equal to nine";
+            
+            Console.WriteLine(response);
         }
     }
 }
